50cfc8fa5a195dbd68882223929644f3f1ce6106
ravilella <lellaravi25ster@gmail.com>
2017-09-16 08:50:39 +0530

    added a new project maven softQ
    added a new project maven softQ

---
+++ MavenSoftQ/src/test/java/com/vmware/MavenSoftQ/BrowserLaunch.java
@@ -0,0 +1,41 @@
+package com.vmware.MavenSoftQ;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch
+{
+	
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		//ChromeDriver driver=new ChromeDriver();
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		driver.navigate().forward();
+		
+		driver.navigate().refresh();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.getPageSource();
+		
+		//driver.close();
+		
+		driver.quit();
+		
+	}
+
+}
